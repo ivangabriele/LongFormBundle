41076c88f1404d3883e6a60201ae557b914eb6f9
Ivan Gabriele <ivan.gabriele@gmail.com>
2015-06-02 13:14:44 +0200

    Test: PhpGenerator::arrayToPhp()

---
+++ Tests/Helper/PhpGeneratorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace InspiredBeings\LongFormBundle\Tests\Helper;
+
+use InspiredBeings\LongFormBundle\Helper\PhpGenerator;
+
+class PhpGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testArrayToPhp()
+    {
+        $result = PhpGenerator::arrayToPhp([
+            'a' => [
+                'b' => true,
+                'f' => 3.14,
+                'i' => 42,
+                's' => "Hello PhpGenerator !",
+            ],
+            'b' => true,
+            'f' => 3.14,
+            'i' => 42,
+            's' => "Hello PhpGenerator !",
+        ], 0, "_EOF_");
+
+        // assert that your calculator added the numbers correctly!
+        $this->assertEquals(
+            "'a' => ['b' => true, 'f' => 3.14, 'i' => 42, 's' => \"Hello PhpGenerator !\", ],_EOF_'b' => true,_EOF_'f' => 3.14,_EOF_'i' => 42,_EOF_'s' => \"Hello PhpGenerator !\",_EOF_",
+            $result
+        );
+    }
+}
